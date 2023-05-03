9c9872be035b98bc1100438ff4326c853a0c69f0
Semi-King <115988342+Semi-King@users.noreply.github.com>
2023-05-03 15:12:24 +0200

    Added initial files

---
+++ app.js
@@ -0,0 +1,9 @@
+// Typing effect
+
+var typingEffect = new Typed(".multiText", {
+  strings: ["Gamer", "designer", "Programmer"],
+  loop: true,
+  typeSpeed: 150,
+  backSpeed: 150,
+  // backDelay : 2000
+});
